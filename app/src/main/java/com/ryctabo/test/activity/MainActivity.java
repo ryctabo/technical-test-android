c2dee571496d63e0d5d4425b34195bce608249b3
Gustavo Pacheco <ryctabo@gmail.com>
2015-12-05 15:42:09 -0500

    Create the toolbar or action bar

--- app/src/main/java/com/ryctabo/test/activity/MainActivity.java
+++ app/src/main/java/com/ryctabo/test/activity/MainActivity.java
@@ -2,13 +2,21 @@ package com.ryctabo.test.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
 }
