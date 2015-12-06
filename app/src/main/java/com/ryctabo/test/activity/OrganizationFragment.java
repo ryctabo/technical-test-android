305a1774d8834f672504f87d33408ba8c8282284
Gustavo Pacheco <ryctabo@gmail.com>
2015-12-06 01:27:00 -0500

    Get data from fragments and implements methods

--- app/src/main/java/com/ryctabo/test/activity/OrganizationFragment.java
+++ app/src/main/java/com/ryctabo/test/activity/OrganizationFragment.java
@@ -4,11 +4,23 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ryctabo.test.entity.Organization;
 
 public class OrganizationFragment extends Fragment {
+
+    EditText editName;
+    EditText editAddress;
+    EditText editPhone;
+
+    Button btnSave;
+    Button btnClean;
 
     public OrganizationFragment() {
     }
@@ -23,6 +35,28 @@ public class OrganizationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_organization, container, false);
+
+        this.editName = (EditText) view.findViewById(R.id.edit_name);
+        this.editAddress = (EditText) view.findViewById(R.id.edit_address);
+        this.editPhone = (EditText) view.findViewById(R.id.edit_phone_number);
+
+        this.btnSave = (Button) view.findViewById(R.id.btnSave);
+        this.btnClean = (Button) view.findViewById(R.id.btnClean);
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionSave();
+            }
+        });
+
+        this.btnClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionClean();
+            }
+        });
+
         return view;
     }
 
@@ -35,4 +69,23 @@ public class OrganizationFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+    public void actionSave() {
+        Log.i("action", "on click in button save.");
+        String name = editName.getText().toString();
+        String address = editAddress.getText().toString();
+        String phone = editPhone.getText().toString();
+
+        Organization organization = new Organization(name, address, phone);
+
+        Log.i("info", organization.toString());
+    }
+
+    public void actionClean() {
+        Log.i("action", "on click in button clean.");
+        this.editName.setText("");
+        this.editAddress.setText("");
+        this.editPhone.setText("");
+    }
+
 }
--- app/src/main/java/com/ryctabo/test/activity/PersonFragment.java
+++ app/src/main/java/com/ryctabo/test/activity/PersonFragment.java
@@ -4,11 +4,23 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ryctabo.test.entity.Person;
 
 public class PersonFragment extends Fragment {
+
+    EditText editFullName;
+    EditText editPhone;
+    EditText editEmail;
+
+    Button btnSave;
+    Button btnClean;
 
     public PersonFragment() {
     }
@@ -23,6 +35,28 @@ public class PersonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_person, container, false);
+
+        this.editFullName = (EditText) view.findViewById(R.id.edit_full_name);
+        this.editPhone = (EditText) view.findViewById(R.id.edit_phone_number);
+        this.editEmail = (EditText) view.findViewById(R.id.edit_email);
+
+        this.btnSave = (Button) view.findViewById(R.id.btnSave);
+        this.btnClean = (Button) view.findViewById(R.id.btnClean);
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionSave();
+            }
+        });
+
+        this.btnClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionClean();
+            }
+        });
+
         return view;
     }
 
@@ -35,4 +69,22 @@ public class PersonFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+    public void actionSave() {
+        Log.i("action", "on click in button save.");
+        String fullName = this.editFullName.getText().toString();
+        String phone = this.editPhone.getText().toString();
+        String email = this.editEmail.getText().toString();
+
+        Person person = new Person(fullName, phone, email);
+        Log.i("info", person.toString());
+    }
+
+    public void actionClean() {
+        Log.i("action", "on click in button clean.");
+        this.editEmail.setText("");
+        this.editFullName.setText("");
+        this.editPhone.setText("");
+    }
+
 }
